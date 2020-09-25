5b02af666b8959504f08f9203315c70bbedd00c3
István Sárándi <sarandi@vision.rwth-aachen.de>
2020-09-25 18:20:46 +0200

    Fix imports

--- pyyolo/__init__.py
+++ pyyolo/__init__.py
@@ -1,2 +1,2 @@
-from yolo_data import BBox, YoloData
-from utils import load_image, load_net, load_meta, classify, detect, array_to_image, load_names
+from pyyolo.yolo_data import BBox, YoloData
+from pyyolo.utils import load_image, load_net, load_meta, classify, detect, array_to_image, load_names
